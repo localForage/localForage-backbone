8f70d08e8c03289e4837f71d110086cbf124b54d
Matthew Riley MacPherson <matt@lonelyvegan.com>
2014-03-16 21:59:05 -0400

    Add code from localForage

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+/*global exports:true, require:true */
+module.exports = exports = function(grunt) {
+    'use strict';
+
+    grunt.initConfig({
+        concat: {
+            options: {
+                separator: '',
+            },
+            backbone: {
+                dest: 'dist/localforage.backbone.js',
+                options: {
+                    banner:
+                        '/*!\n' +
+                        '    localForage Backbone Adapter\n' +
+                        '    Version 0.1.0\n' +
+                        '    https://github.com/mozilla/localforage-backbone\n' +
+                        '    (c) 2014 Mozilla, Apache License 2.0\n' +
+                        '*/\n'
+                },
+                src: [
+                    'src/localforage.backbone.js'
+                ]
+            }
+        },
+        uglify: {
+            backbone: {
+                files: {
+                    'dist/localforage.backbone.min.js': ['dist/localforage.backbone.js']
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-casper');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', ['build']);
+    grunt.registerTask('build', ['concat', 'uglify']);
+
+    grunt.registerTask('test', ['build']);
+};
