ce6297962f1565b41da9db8e7999aba8b9989a29
Stéphane Bachelier <stephane.bachelier@gmail.com>
2015-02-26 16:38:30 +0100

    feat(example): add footer to inform about driver support
    Switching between drivers is enabled.

--- examples/main.js
+++ examples/main.js
@@ -134,6 +134,54 @@
         }
     });
 
+    var drivers = [
+        'LOCALSTORAGE',
+        'WEBSQL',
+        'INDEXEDDB'
+    ];
+
+    var FooterView = Backbone.View.extend({
+        template: _.template($('#footertpl').html()),
+
+        events: {
+            'click .tab-item': 'onTabItemChange'
+        },
+
+        render: function () {
+            this.$el.html(this.template());
+            this.showSupport();
+            this.showActiveDriver();
+        },
+
+        showSupport: function () {
+            drivers.map(_.bind(this.updateDriverIcon, this));
+        },
+
+        showActiveDriver: function () {
+            drivers.map(_.bind(this.updateDriverTabItem, this));
+        },
+
+        updateDriverTabItem: function (driverName) {
+            var method = localforage.driver() === localforage[driverName] ? $.fn.addClass: $.fn.removeClass;
+            var $el = this.$('[data-item=' + driverName + ']');
+            method.call($el, 'active');
+        },
+
+        updateDriverIcon: function (driverName) {
+            var isSupported = localforage.supports(localforage[driverName]);
+            var className = isSupported ? 'icon-check' : 'icon-close';
+            this.$('[data-item=' + driverName + '] > .icon').addClass(className);
+        },
+
+        onTabItemChange: function (event) {
+            event.preventDefault();
+            var driverName = $(event.currentTarget).data('item');
+            localforage.setDriver(localforage[driverName]).then(
+                _.bind(this.showActiveDriver, this)
+            );
+        }
+    });
+
     // Instancing the collection and the view
     var collection = new ListCollection();
 
@@ -145,6 +193,12 @@
     var listView = new ListView({
         collection: collection
     });
+
+    var footerView = new FooterView({
+        el: $('footer')
+    });
+
+    footerView.render();
 
     formView.render();
     $('.content').append(listView.render().el);
