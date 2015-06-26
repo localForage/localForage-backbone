f535cc40805f378b2e7afb2eec562c7a9cd5a270
T1st3 <contact@tiste.org>
2015-06-26 17:25:56 +0200

    fix indentation in examples/main

--- examples/main.js
+++ examples/main.js
@@ -87,36 +87,36 @@
     });
 
     var ListView = Backbone.View.extend({
-      tagName: 'ul',
-      className: 'table-view',
+        tagName: 'ul',
+        className: 'table-view',
 
-      initialize: function() {
-          this.listenTo(this.collection, 'add', this.addItemView);
-          this.listenTo(this.collection, 'remove', this.removeItem);
-          this.listenTo(this.collection, 'reset', this.reset);
-          this._itemsView = {};
-      },
+        initialize: function() {
+            this.listenTo(this.collection, 'add', this.addItemView);
+            this.listenTo(this.collection, 'remove', this.removeItem);
+            this.listenTo(this.collection, 'reset', this.reset);
+            this._itemsView = {};
+        },
 
-      addItemView: function(model) {
-          var itemView = new ItemView({model: model});
-          this.$el.append(itemView.render().el);
-          this._itemsView[model.id] = itemView;
-      },
+        addItemView: function(model) {
+            var itemView = new ItemView({model: model});
+            this.$el.append(itemView.render().el);
+            this._itemsView[model.id] = itemView;
+        },
 
-      removeItem: function(model) {
-          this._itemsView[model.id].remove();
-          this._itemsView[model.id] = null;
-          delete this._itemsView[model.id];
-      },
+        removeItem: function(model) {
+            this._itemsView[model.id].remove();
+            this._itemsView[model.id] = null;
+            delete this._itemsView[model.id];
+        },
 
-      reset: function(model, options) {
-          options.previousModels.map(this.removeItem, this);
-      },
+        reset: function(model, options) {
+            options.previousModels.map(this.removeItem, this);
+        },
 
-      render: function() {
-          this.collection.map(this.addItemView, this);
-          return this;
-      }
+        render: function() {
+            this.collection.map(this.addItemView, this);
+            return this;
+        }
     });
 
     var ItemView = Backbone.View.extend({
